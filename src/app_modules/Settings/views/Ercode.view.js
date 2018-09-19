4982e5541b0eed4fe67eb93d8b47d7ed1760ff41
shibo <576504437@q.com>
2018-09-19 15:30:27 +0800

    qr code

--- src/app_modules/Settings/views/Ercode.view.js
+++ src/app_modules/Settings/views/Ercode.view.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { Button, Card } from "antd"
-import NestedComponent from 'app/decorators/NestedComponent'
 import PageView from 'app/components/Page'
 import DataTable from 'app/components/DataTable'
 import ButtonGroupExt from 'app/components/ButtonGroupExt'
 import ButtonGroups from 'app/components/ButtonGroups'
+import API from '../api'
+import WrapperComponent from 'app/decorators/WrapperComponent'
+import ModalView,{ModalWidthView,ModalDetailView} from 'app/components/Modal.view'
 
 
-@NestedComponent()
+@WrapperComponent(ModalView)
 export default class Ercode extends PageView {
 	render(){
-		return <div>二维码页面</div>
+		return <img src='/getQRCode' style={{width:200,display:"block",margin:"0 auto"}}/>
 	}
 }
