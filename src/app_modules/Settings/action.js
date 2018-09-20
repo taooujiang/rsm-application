bb75df333968331f7b375a8781d47da9143aa3f3
zwlafk <wlongtony@gmail.com>
2018-09-20 17:12:22 +0800

    :bug:

--- src/app_modules/Settings/action.js
+++ src/app_modules/Settings/action.js
@@ -236,7 +236,7 @@ export function saveRemindAction(params) {
 		dispatch(fetchRequest('remind'))
 		processBool2Str(params)
 		return new API().fetchSaveRemind(params).then(json => {
-			dispatch(fetchSuccess('remind'))
+			dispatch(fetchSuccess('remind', true))
 			dispatch(remindSave(Object.assign({id:'account'},params)))
 		}).catch(ex => {
 			return dispatch(fetchFailure('remind', ex))
