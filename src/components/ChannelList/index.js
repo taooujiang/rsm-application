aa2d0ac2ab543f5d56dac5d97f2e29727c2a3e87
alex_tao <taooujiang@126.com>
2018-09-28 10:41:50 +0800

    图标链接

--- src/components/ChannelList/index.js
+++ src/components/ChannelList/index.js
@@ -11,6 +11,7 @@ class ChannelList extends Component {
 		if(dataSource&&sortKey){
 			dataSource=dataSource.sort((a,b)=>a[sortKey]-b[sortKey])
 		}
+		console.log("dataSource",dataSource)
 		return(
 			<List
 				header={headerTitle?headerTitle:null}
@@ -19,7 +20,7 @@ class ChannelList extends Component {
 				dataSource={dataSource}
 				renderItem={item => (
 					<List.Item style={{textAlign:'center'}}>
-						<div style={{margin:'0 auto'}} className={"channel-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.keyURl)}></div>
+						<div style={{margin:'0 auto'}} className={"channel-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.keyURl2)}></div>
 						<div>{item[keyName]?keyStr:''}</div>
 					</List.Item>
 				)}
