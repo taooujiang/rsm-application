2c149dd5631a9f599f4a126b949b4ce67a3bd8ab
shibo <576504437@q.com>
2018-10-24 09:35:48 +0800

    channel login url

--- src/components/ChannelList/index.js
+++ src/components/ChannelList/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { List } from "antd";
+import DictUtils from 'app-utils/DictUtils'
 import style from './style.less'
 class ChannelList extends Component {
-	loginChannel(keyURl){
-		global.invokeMethod('ShowPublicUrl',keyURl)
+	loginChannel(keyValue){
+		let keyUrl = DictUtils.getDictItemByValue("channel",keyValue).pop().keyURl2
+		// console.log(keyUrl)
+		global.invokeMethod('ShowPublicUrl',keyUrl)
 	}
 	render() {
 		const{keyName,keyStr,channelKeyName,headerTitle,sortKey,loginStatus}=this.props
@@ -21,10 +24,10 @@ class ChannelList extends Component {
 				renderItem={item => (
 					<List.Item style={{textAlign:'center'}}>
 						{loginStatus ?
-							<div><div style={{margin:'0 auto'}} className={item.isLogin ? "channel-img-"+ item[channelKeyName] : "channel-logout-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.keyURl2)}></div>
+							<div><div style={{margin:'0 auto'}} className={item.isLogin ? "channel-img-"+ item[channelKeyName] : "channel-logout-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.id)}></div>
 							<div>{item[keyName]?keyStr:''}</div></div>
 							:
-							<div><div style={{margin:'0 auto'}} className={"channel-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.keyURl2)}></div>
+							<div><div style={{margin:'0 auto'}} className={"channel-img-"+ item[channelKeyName]} onClick={this.loginChannel.bind(this,item.keyValue)}></div>
 							<div>{item[keyName]?keyStr:''}</div></div>
 						}
 					</List.Item>
