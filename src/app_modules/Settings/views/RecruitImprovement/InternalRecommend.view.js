407d3fba10351153916daf69c0f85f2cad3c76fc
zwlafk <wlongtony@gmail.com>
2018-11-29 13:13:58 +0800

    :rocket: InternalRecommend

--- src/app_modules/Settings/views/RecruitImprovement/InternalRecommend.view.js
+++ src/app_modules/Settings/views/RecruitImprovement/InternalRecommend.view.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import NestedComponent from 'app/decorators/NestedComponent'
-import { Button, Card, Input, Divider, message, Select, InputNumber } from "antd"
+import { Button, Card, Input, Divider, message, Select, InputNumber, Spin } from "antd"
 import BaseForm, { FormItem, customRules } from "components/BaseForm";
+// 不用DataTable 是因为min-height有差异
 import EditableTable from "components/TableForm";
 import { FormPage } from "app/components/Page";
-import { fetchInternalRecommend } from '../../api'
+import { fetchInternalRecommend, saveInternalRecommend } from '../../api'
 import './style.less';
 
 const Option = Select.Option;
@@ -13,6 +14,13 @@ export default class InternalRecommend extends FormPage {
   state = {
     rewardList: [],
     levelRewardList: [],
+    isDomainNameChanged: false,
+    loading: false
+  }
+  setLoading(bool) {
+    this.setState({
+      loading: bool
+    })
   }
   renderToolbar() {
     return (
@@ -20,8 +28,11 @@ export default class InternalRecommend extends FormPage {
     )
   }
   componentDidMount = () => {
+    this.setLoading(true)
     fetchInternalRecommend().then(res => {
-      const { awardBeans, positionLevelBeans } = res
+      this.setLoading(false)
+
+      const { awardBeans, positionLevelBeans, domainName } = res
       let rewardList = awardBeans.map(e => {
         const { id, domainName, awardUnit, awardType, awardNum, stage } = e
         return { id, domainName, awardUnit, awardType, awardNum, stage }
@@ -32,27 +43,42 @@ export default class InternalRecommend extends FormPage {
       })
       this.setState({
         rewardList,
-        levelRewardList
+        levelRewardList,
+        domainName
       })
     }).catch(e => {
-      message.warning(e.msg)
+      message.error(e.msg)
+      this.setLoading(false)
     })
   }
   handleSubmit(values) {
-    console.log(values, 'ssssssssss')
-    // let { actions, router } = this.props;
-    // this.setState({
-    //   loading: true
-    // })
-    // saveShareSetting(values).then(res => {
-    //   message.success('保存成功');
-    //   this.setState({
-    //     loading: false
-    //   })
-    // })
+    let { levelRewardList, rewardList, } = this.state
+    if (this.state.isDomainNameChanged) {
+      rewardList = rewardList.map(e => {
+        return {
+          ...e,
+          domainName: this.state.domainName
+        }
+      })
+    }
+    this.setLoading(true)
+    saveInternalRecommend({
+      sysSetInterpolateAwardBeanList: rewardList,
+      sysPositionLevelBeans: levelRewardList
+    }).then(res => {
+      this.setLoading(false)
+      message.success('保存成功');
+    }).catch(e => {
+      this.setLoading(false)
+      message.error(e.msg);
+    })
   }
+  // todo,domainName -->state;when submit, diff them
   handleHostnameChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      isDomainNameChanged: true,
+      domainName: e.target.value
+    })
   }
 
   handleTableFormItemChange(type, formItemType, record, val) {
@@ -71,14 +97,14 @@ export default class InternalRecommend extends FormPage {
     this.setState({
       [listMapper[type]]: tempDataList
     })
-    this[type].props.onChange(tempDataList)
+    // this[type].props.onChange(tempDataList)
   }
-  onRewardRef = (ref) => {
-    this.rewardForm = ref
-  }
-  onLevelRewardRef = (ref) => {
-    this.levelRewardForm = ref
-  }
+  // onRewardRef = (ref) => {
+  //   this.rewardForm = ref
+  // }
+  // onLevelRewardRef = (ref) => {
+  //   this.levelRewardForm = ref
+  // }
   renderRewardSettingTable() {
     const columns = [{
       title: '阶段',
@@ -92,7 +118,7 @@ export default class InternalRecommend extends FormPage {
       render: (text, record) => (
         <div>
           <span>达到 </span>
-          <InputNumber style={{ width: 80 }} onChange={this.handleTableFormItemChange.bind(this, 'rewardForm', 'awardUnit', record)} />
+          <InputNumber style={{ width: 80 }} defaultValue={text} onChange={this.handleTableFormItemChange.bind(this, 'rewardForm', 'awardUnit', record)} />
           <span> 次</span>
         </div>
       ),
@@ -102,8 +128,8 @@ export default class InternalRecommend extends FormPage {
       key: 'awardType',
       render: (text, record) => (
         <Select defaultValue={text} style={{ width: 120 }} onChange={this.handleTableFormItemChange.bind(this, 'rewardForm', 'awardType', record)} >
-          <Option value="1" >积分</Option>
-          <Option value="2">现金</Option>
+          <Option value={1}>积分</Option>
+          <Option value={2}>现金</Option>
         </Select>
       ),
     }, {
@@ -115,7 +141,9 @@ export default class InternalRecommend extends FormPage {
       ),
     }];
     return (<FormItem>
-      <EditableTable onRef={this.onRewardRef} columns={columns} data={this.state.rewardList} label="内推奖励设置" name="sysSetInterpolateAwardBeanList" />
+      <EditableTable
+        // onRef={this.onRewardRef}
+        columns={columns} data={this.state.rewardList} label="内推奖励设置" name="sysSetInterpolateAwardBeanList" />
     </FormItem>)
   }
 
@@ -130,8 +158,8 @@ export default class InternalRecommend extends FormPage {
       key: 'awardType',
       render: (text, record) => (
         <Select defaultValue={text} style={{ width: 120 }} onChange={this.handleTableFormItemChange.bind(this, 'levelRewardForm', 'awardType', record)} >
-          <Option value="1">积分</Option>
-          <Option value="2">现金</Option>
+          <Option value={1}>积分</Option>
+          <Option value={2}>现金</Option>
         </Select>
       ),
     }, {
@@ -139,11 +167,13 @@ export default class InternalRecommend extends FormPage {
       dataIndex: 'awardNum',
       key: 'awardNum',
       render: (text, record) => (
-        <InputNumber style={{ width: 120 }} onChange={this.handleTableFormItemChange.bind(this, 'levelRewardForm', 'awardNum', record)} />
+        <InputNumber style={{ width: 120 }} defaultValue={text} onChange={this.handleTableFormItemChange.bind(this, 'levelRewardForm', 'awardNum', record)} />
       ),
     },];
     return (<FormItem>
-      <EditableTable onRef={this.onLevelRewardRef} columns={columns} data={this.state.levelRewardList} label="特殊职位奖励设置" name="sysPositionLevelBeans" />
+      <EditableTable
+        // onRef={this.onLevelRewardRef}
+        columns={columns} data={this.state.levelRewardList} label="特殊职位奖励设置" name="sysPositionLevelBeans" />
     </FormItem>)
   }
 
@@ -160,20 +190,21 @@ export default class InternalRecommend extends FormPage {
     };
     return (
       <Card title={<div><h3 className="card-title">内推设置</h3></div>} extra={this.renderToolbar()}	>
+        <Spin spinning={this.state.loading}>
+          <BaseForm
+            onSubmit={handleSubmit}
+            ref={this.saveFormRef}>
+            <FormItem {...formFullItemLayout} className="host-item">
+              <Input addonBefore="@" label="内推邮箱域名设置" defaultValue={this.state.domainName} name="hostName" onChange={this.handleHostnameChange.bind(this)} />
+            </FormItem>
+            <p>设置后，符合域名条件的邮箱投递的简历，会自动识别为内推邮件。投递简历时，简历邮件主题必须为含有“【应聘简历】”。</p>
+            <Divider />
 
-        <BaseForm
-          onSubmit={handleSubmit}
-          ref={this.saveFormRef}>
-          <FormItem {...formFullItemLayout} className="host-item">
-            <Input addonBefore="@" label="内推邮箱域名设置" name="hostName" onChange={this.handleHostnameChange.bind(this)} />
-          </FormItem>
-          <p>设置后，符合域名条件的邮箱投递的简历，会自动识别为内推邮件。投递简历时，简历邮件主题必须为含有“【应聘简历】”。</p>
-          <Divider />
+            {this.renderRewardSettingTable()}
+            {this.renderLevelRewardSettingTable()}
 
-          {this.renderRewardSettingTable()}
-          {this.renderLevelRewardSettingTable()}
-
-        </BaseForm>
+          </BaseForm>
+        </Spin>
       </Card>
     )
   }
