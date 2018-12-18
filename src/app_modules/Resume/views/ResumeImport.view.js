bfb5e07adf3c5683c0fbadf3f0d4fdd7689def20
shibo <576504437@q.com>
2018-12-18 15:25:07 +0800

    resume import styles && rules

--- src/app_modules/Resume/views/ResumeImport.view.js
+++ src/app_modules/Resume/views/ResumeImport.view.js
@@ -49,12 +49,15 @@ export default class ResumeImportView extends FormPage {
   }
   handleSubmit(){
     let {actions} = this.props
-    this.form.validateFieldsAndScroll({force:true},(err,values) => {
+    let importform = this.form
+    importform.validateFieldsAndScroll({force:true},(err,values) => {
        if (err) {
          return;
        }
        // console.log(values)
-       actions.importResumeAction(values)
+       actions.importResumeAction(values).then(json=>{
+         importform.resetFields()
+       })
      });
   }
   renderSelectOption(data,idx){
@@ -72,31 +75,58 @@ export default class ResumeImportView extends FormPage {
       return (
         <BaseForm ref={this.saveFormRef} className="resume-import-form">
           <Layout direction='rows' className="resume-import">
-              <Pane>
-                <FormItem>
+              <Pane className="fileUploadArea">
+                <FormItem className="fileUploadItem">
                   <ImgUpload type={6} name="sourceFileId" btnText="点击上传候选人简历" beforeUpload={this.beforeUpload} onResponse={this.fileUploadResponse} onSuccess={this.fileUploadSuccess}></ImgUpload>
                 </FormItem>
             </Pane>
               <Fixed style={{width:500}}>
                 <FormItem>
-                  <Input label="姓名" name="name" placeholder = "请填写姓名"/>
+                  <Input label="姓名" name="name" placeholder = "请填写姓名" rules={[
+											{
+												required: true,
+												message: "姓名不可为空"
+											}, {
+												validator: customRules.required
+											}
+										]}/>
                 </FormItem>
                 <FormItem>
-                  <Select label="性别" name="sex" placeholder = "请选择" fetch={DictUtils.getDictByType("sex")} renderItem={this.renderSelectOption}></Select>
+                  <Select label="性别" name="sex" placeholder = "请选择" fetch={DictUtils.getDictByType("sex")} renderItem={this.renderSelectOption} rules={[
+											{
+												required: true,
+												message: "性别不可为空"
+											}, {
+												validator: customRules.required
+											}
+										]}></Select>
                 </FormItem>
                 <FormItem>
-                  <Input label="电话号码" name="mobilephone" placeholder = "请填写电话号码"/>
+                  <Input label="电话号码" name="mobilephone" placeholder = "请填写电话号码" rules={[
+											{
+												required: true,
+												message: "电话号码不可为空"
+											}, {
+												validator: customRules.checkMobile
+											}, {
+												validator: customRules.required
+											}
+										]}/>
                 </FormItem>
                 <FormItem>
-                  <Input label="邮箱号码" name="email" placeholder = "请填写邮箱号码"/>
+                  <Input label="邮箱号码" name="email" placeholder = "请填写邮箱号码" rules={[{
+												type: "email",
+												message: "邮箱格式不正确"
+											}
+										]}/>
                 </FormItem>
                 <FormItem>
-                  <Select label="关联职位" name="jobId" placeholder = "请选择" fetch={`${APP_SERVER}/jobNew/getJobList`} renderItem={this.renderJobOption}></Select>
+                  <Select label="关联职位" name="jobId" placeholder = "请选择" fetch={`${APP_SERVER}/jobNew/getJobList`} renderItem={this.renderJobOption} ></Select>
                 </FormItem>
                 <FormItem>
                   <Select label="推荐人" name="referrer" placeholder = "请选择" fetch = {`${APP_SERVER}/member/findMemebersWithOnJob`} renderItem = {this.renderRefferOption }/>
                 </FormItem>
-                <Button onClick={this.handleSubmit.bind(this)}>提交</Button>
+                <Button type="primary" onClick={this.handleSubmit.bind(this)}>提交</Button>
               </Fixed>
           </Layout>
         </BaseForm>
